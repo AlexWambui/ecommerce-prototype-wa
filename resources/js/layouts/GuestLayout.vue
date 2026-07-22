907e2d901733bdcc167b71830f4a816bb66c1c53
AlexAaqil <alexwambuik@gmail.com>
2026-07-22 22:12:56 +0300

    database setup as well as the navbar and basic home page rendering.

---
+++ resources/js/layouts/GuestLayout.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import Navbar from '@/components/custom/Navbar/Index.vue';
+import { Toaster } from '@/components/ui/sonner';
+</script>
+
+<template>
+    <Toaster />
+    
+    <Navbar />
+    
+    <main class="GuestLayout">
+        <slot />
+    </main>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\HomePageController;
 
-Route::inertia('/', 'Welcome')->name('home');
+Route::inertia('/welcome', 'Welcome')->name('welcome-page');
+
+Route::get('/', [HomePageController::class, 'index'])->name('home');
+
+Route::get('/users', [UserController::class, 'index'])->name('users.index');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'Dashboard')->name('dashboard');
